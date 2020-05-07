dbda9b672947e889683195d7c7de3ebf4b3b4fdd
Affan <affan_94@hotmail.com>
2020-05-07 18:38:45 +0500

    cron added

--- portal/resources/views/pages/customer/includes/verification_content.blade.php
+++ portal/resources/views/pages/customer/includes/verification_content.blade.php
@@ -106,15 +106,6 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
 
             </div>
         </div>
