220a86f43a2046354ae7d3dceb5597935c4210fd
affan <affan_94@hotmail.com>
2020-11-19 22:55:52 +0500

    some url changes

--- portal/routes/web.php
+++ portal/routes/web.php
@@ -12,59 +12,62 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/about', 'HomeController@about')->name('about');
 Route::get('/contact', 'ContactController@index')->name('contact');
 Route::get('/test', 'TestController@index')->name('test');
 Route::get('file/{authid}/{location}/{filename}', 'HelperController@getContentFile')->where([
-    'filename'=> '^[^/]+$'
-    ])->name('content');
+    'filename' => '^[^/]+$'
+])->name('content');
 
 Route::get('/test/user-info', "TestController@getUserRole");
 
 
 // Customer Routes
 
-Route::middleware(['customer'])->group(function (){
+Route::middleware(['customer'])->group(function () {
 
-    Route::get('/customer/dashboard', 'CustomerController@index')->name('customer-dashboard');
-    Route::get('/customer/edit-profile', 'CustomerController@editProfile')->name('customer-edit-profile');
+    Route::prefix('customer')->group(function () {
 
-//    addresslog
-    Route::resources([
-        'address-log' => 'AddresslogController',
-        'parcel' => 'ParcelController'
-    ]);
+        Route::get('dashboard', 'CustomerController@index')->name('customer-dashboard');
+        Route::get('edit-profile', 'CustomerController@editProfile')->name('customer-edit-profile');
 
-// Post Routes
-Route::post('/customer/file-upload-bill', 'CustomerController@fileUploadBill')->name('file-upload-bill');
-Route::post('/customer/file-upload-cnic', 'CustomerController@fileUploadCnic')->name('file-upload-cnic');
-Route::post('/customer/proceed-verification', 'CustomerController@proceedVerification')->name('customer-verification-proceed');
-Route::post('/customer/proceed-business-information', 'CustomerController@proceedBusinessInformation')->name('customer-business-information-proceed');
-Route::post('/parcel/get-consignee', 'ParcelController@getConsignee')->name('parcel-get-consignee');
+        //    addresslog
+        Route::resources([
+            'address-log' => 'AddresslogController',
+            'parcel' => 'ParcelController'
+        ]);
 
+        // Post Routes
+        Route::post('file-upload-bill', 'CustomerController@fileUploadBill')->name('file-upload-bill');
+        Route::post('file-upload-cnic', 'CustomerController@fileUploadCnic')->name('file-upload-cnic');
+        Route::post('proceed-verification', 'CustomerController@proceedVerification')->name('customer-verification-proceed');
+        Route::post('proceed-business-information', 'CustomerController@proceedBusinessInformation')->name('customer-business-information-proceed');
+    });
+
+    Route::post('/parcel/get-consignee', 'ParcelController@getConsignee')->name('parcel-get-consignee');
 });
 
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->group(function () {
 
     Route::get('login', 'AdminLoginController@showLoginForm')->name('admin-users');
     Route::post('login/owner', 'AdminLoginController@login')->name('admin-owner-login');
-    Route::middleware(['admin'])->group(function (){
+    Route::middleware(['admin'])->group(function () {
         Route::get('dashboard', 'AdminController@index')->name('admin-dashboard');
 
         // users routes
         Route::post('file-upload-bill', 'CustomerController@fileUploadBill')->name('file-upload-bill');
         Route::post('file-upload-cnic', 'CustomerController@fileUploadCnic')->name('file-upload-cnic');
-        Route::match(['get', 'post'],'user/{action}/{id?}/{form_name?}', 'AdminController@user')->name('admin-user');
+        Route::match(['get', 'post'], 'user/{action}/{id?}/{form_name?}', 'AdminController@user')->name('admin-user');
 
         //parcel route
-        Route::match(['get', 'post'],'parcel/{action}/{id?}/{form_name?}', 'AdminController@parcel')->name('admin-parcel');
+        Route::match(['get', 'post'], 'parcel/{action}/{id?}/{form_name?}', 'AdminController@parcel')->name('admin-parcel');
 
-        Route::match(['get', 'post'],'csv/upload', 'AdminController@csv')->name('admin-csv');
+        Route::match(['get', 'post'], 'csv/upload', 'AdminController@csv')->name('admin-csv');
         Route::get('ajax/customers', 'AdminController@ajaxUsersList')->name('admin-customers-list');
     });
-
 });
 
 
